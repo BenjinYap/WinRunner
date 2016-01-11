4dd2cb5c6619b4fbf9f15eceecdd06bb59060d9f
Benjin Yap <benjyap.dev@gmail.com>
2016-01-10 19:40:50 -0500

    added helper class

---
+++ WinRunner/Models/RegistryHelper.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.Win32;
+namespace WinRunner.Models {
+	public static class RegistryHelper {
+
+		public static RegistryKey OpenAppPaths () {
+			return Registry.CurrentUser.CreateSubKey (@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths");
+		}
+	}
+}
