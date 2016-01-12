ba8d7fbb97d444555ce1b43590e3329b02122ad6
Benjin Yap <benjyap.dev@gmail.com>
2016-01-12 16:23:44 -0500

    forgot to add file for previous commit

---
+++ WinRunner/Views/CenteredDialogWindow.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+using System.Windows;
+namespace WinRunner.Views {
+	public class CenteredDialogWindow:Window {
+
+		protected override void OnContentRendered (EventArgs e) {
+			base.OnContentRendered (e);
+
+			this.Left = this.Owner.Left + this.Owner.Width / 2 - this.Width / 2;
+			this.Top = this.Owner.Top + this.Owner.Height / 2 - this.Height / 2;
+		}
+	}
+}
