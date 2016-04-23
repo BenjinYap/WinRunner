a110443792d5a1c2a6b44bd3d4bc962b338fa26d
Benjin Yap <benjyap.dev@gmail.com>
2016-04-23 19:45:35 -0400

    added edge shortcut, on a plane in the air!

---
+++ WinRunner/Models/Shortcuts/MSEdgeShortcut.cs
@@ -0,0 +1,97 @@
+﻿
+
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+using WinRunner.Resources;
+namespace WinRunner.Models.Shortcuts {
+	public class MSEdgeShortcut:FileShortcut {
+		private const string UrlKeyName = "Url";
+
+		private string url;
+		public string Url {
+			get { return this.url; }
+			set {
+				this.url = value;
+				this.ValidateUrl ();
+				base.OnPropertyChanged ();
+			}
+		}
+
+		private string oldUrl;
+
+		private readonly static string WebPageFolderPath = System.IO.Path.Combine (Shortcut.DocumentsPath, "WebPageScripts");
+
+		private string script {
+			get { return string.Format ("start \"{0}\" {1}", this.Path, this.Url); }
+		}
+
+		private string scriptPath {
+			get { return System.IO.Path.Combine (MSEdgeShortcut.WebPageFolderPath, this.Name + ".bat"); }
+		}
+
+		public MSEdgeShortcut ():base () {
+			this.Url = "";
+		}
+
+		public MSEdgeShortcut (RegistryKey regKey):base (regKey) {
+			this.Url = regKey.GetValue (MSEdgeShortcut.UrlKeyName).ToString ();
+		}
+
+		public override void RememberProperties () {
+			base.RememberProperties ();
+			this.oldUrl = this.Url;
+		}
+
+		public override void RevertProperties () {
+			base.RevertProperties ();
+			this.Url = this.oldUrl;
+		}
+
+		public override void FlushToRegistry () {
+			base.FlushToRegistry ();
+
+			//set the exe path to the batch file
+			this.regKey.SetValue ("", this.scriptPath);
+
+			//save the path of the folder to be opened
+			this.regKey.SetValue (MSEdgeShortcut.UrlKeyName, this.Url);
+
+			//create the batch file
+			this.FlushToScript ();
+		}
+
+		public override void DeleteFromRegistry () {
+			base.DeleteFromRegistry ();
+			File.Delete (this.scriptPath);
+			File.Delete (System.IO.Path.Combine (MSEdgeShortcut.WebPageFolderPath, this.oldName + ".bat"));
+		}
+
+		private void FlushToScript () {
+			FileInfo fileInfo = new FileInfo (this.scriptPath);
+			fileInfo.Directory.Create ();
+			File.WriteAllText (fileInfo.FullName, this.script);
+		}
+
+		private void ValidateUrl ([CallerMemberName] string propertyName = null) {
+			if (this.Url.Length <= 0) {
+				base.AddError (General.Required, propertyName);
+			} else {
+				base.RemoveError (General.Required, propertyName);
+
+				Uri uri = null;
+
+				if (Uri.TryCreate (this.Url, UriKind.Absolute, out uri)) {
+					base.RemoveError (General.Invalid, propertyName);
+				} else {
+					base.AddError (General.Invalid, propertyName);
+				}
+			}
+		}
+	}
+}
