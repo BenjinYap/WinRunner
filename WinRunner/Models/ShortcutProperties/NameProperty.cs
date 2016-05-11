d855548cdaf265e451dd9b62f18d21ade07f82ac
Benjin Yap <benjyap.dev@gmail.com>
2016-05-11 00:04:15 -0400

    comment

--- WinRunner/Models/ShortcutProperties/NameProperty.cs
+++ WinRunner/Models/ShortcutProperties/NameProperty.cs
@@ -16,10 +16,12 @@ namespace WinRunner.Models.ShortcutProperties {
 		}
 
 		protected override void Validate () {
+			//get all the existing names from the registry
 			RegistryKey rootKey = RegistryHelper.OpenAppPaths ();
 			string [] appKeyNames = rootKey.GetSubKeyNames ();
 			rootKey.Close ();
 			
+			//check if any of the existing names conflict with the current name
 			foreach (string keyName in appKeyNames) {
 				if (this.GetAppName (keyName.ToLower ()) == this.Value.ToLower () && (this.regKey == null || this.GetAppName (this.regKey.Name.ToLower ()) != this.Value.ToLower ())) {
 					this.Errors [General.NameExists] = true;
@@ -29,24 +31,28 @@ namespace WinRunner.Models.ShortcutProperties {
 				}
 			}
 			
+			//check required
 			if (this.Value.Length <= 0) {
 				this.Errors [General.NameRequired] = true;
 			} else {
 				this.Errors [General.NameRequired] = false;
 			}
 
+			//check backslash
 			if (this.Value.Contains (@"\")) {
 				this.Errors [General.NameInvalidBackslash] = true;
 			} else {
 				this.Errors [General.NameInvalidBackslash] = false;
 			}
 
+			//check space
 			if (this.Value.Contains (" ")) {
 				this.Errors [General.NameInvalidSpace] = true;
 			} else {
 				this.Errors [General.NameInvalidSpace] = false;
 			}
 
+			//check dot
 			if (this.Value.Contains (".")) {
 				this.Errors [General.NameInvalidDot] = true;
 			} else {
